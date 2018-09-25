2aa49564968abe80a7a57c7d328934429e87fd75
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-09-25 17:30:43 -0500

    dependientes

---
+++ routes/dependientesRoutes.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const Dependiente = mongoose.model('dependientes');
+
+module.exports = (app, escogerBdd) => {
+
+	app.get('/api/dependientes/:equipo', async (req, res) => {
+		await escogerBdd(req.params.equipo);
+
+		const dependientes = await Dependiente.find({});
+		res.send(dependientes);
+	});
+
+//=========================================================================
+
+	app.get('/api/dependientes/:equipo/:id', async (req, res) => {
+		await escogerBdd(req.params.equipo);
+
+		const dependientes = await Dependiente.find({ _id: req.params.id });
+		res.send(dependientes);
+	});
+
+//=========================================================================
+
+	app.post('/api/dependientes/:equipo', async (req, res) => {
+		await escogerBdd(req.params.equipo);
+
+		const { nombre_completo, edad, _usuario, dependencia } = req.body;
+
+		if (!nombre_completo) res.send('Falta el nombre completo.');
+		if (!dependencia) res.send('Falta dependencia.');
+		if (!edad) res.send('Falta la edad.');
+		if (!_usuario) res.send('Falta ID de usuario.');
+
+		const dependiente = new Dependiente({
+			nombre_completo, edad, _usuario, dependencia
+		});
+		const respuesta = await dependiente.save();
+
+		res.send(respuesta);
+	});
+
+//=========================================================================
+
+	app.post('/api/dependientes/:equipo/:id', async (req, res) => {
+		await escogerBdd(req.params.equipo);
+
+		const { nombre_completo, edad, _usuario, dependencia } = req.body;
+
+		if (!nombre_completo) res.send('Falta el nombre completo.');
+		if (!dependencia) res.send('Falta dependencia.');
+		if (!edad) res.send('Falta la edad.');
+		if (!_usuario) res.send('Falta ID de usuario.');
+
+		const respuesta = await Dependiente.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ nombre, apellidos, edad },
+			{ new: true }
+		).exec();
+
+		res.send(respuesta);
+	});
+
+//=========================================================================
+
+	app.delete('/api/dependientes/:equipo/:id', async (req, res) => {
+		await escogerBdd(req.params.equipo);
+
+		const dependientes = await Dependiente.deleteOne({ _id: req.params.id });
+		res.send(dependientes);
+	});
+
+};
