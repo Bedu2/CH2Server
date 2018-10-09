a1b39da86d18738f3e86f4dd4e268b11b51ccb8c
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-10-09 13:53:11 -0500

    1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Methods', 'get, post, delete');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
