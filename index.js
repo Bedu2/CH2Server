ce7076648016827f109b0aa24006fbe9680797a3
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-10-09 12:59:47 -0500

    todo

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ require('./models/Dependiente');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
