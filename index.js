ce1e2525cd44f5fcba4ba71aa21d70a1496ea5d1
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-10-09 13:23:05 -0500

    1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-var cors = require('cors');
 require('./models/Usuario');
 require('./models/Dependiente');
 
@@ -10,26 +9,12 @@ require('./models/Dependiente');
 
 const app = express();
 app.use(bodyParser.json());
-// Add headers
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	next();
 });
-app.use(cors({origin: '*'}));
 
 const escogerBdd = (equipo) => {
 	switch (equipo) {
