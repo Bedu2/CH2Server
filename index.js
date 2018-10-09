7b1418d073f909b140b2108a20c3f4d93072a912
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.local>
2018-10-09 13:49:05 -0500

    como antes

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ require('./models/Dependiente');
 // mongoose.connect(keys.mongoRed);
 
 const app = express();
-const aceptarCors = (req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-}
 app.use(bodyParser.json());
-app.use(aceptarCors);
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 const escogerBdd = (equipo) => {
 	switch (equipo) {
@@ -25,8 +24,8 @@ const escogerBdd = (equipo) => {
 	}
 };
 
-require('./routes/dependientesRoutes')(app, escogerBdd, aceptarCors);
-require('./routes/usuariosRoutes')(app, escogerBdd, aceptarCors);
+require('./routes/usuariosRoutes')(app, escogerBdd);
+require('./routes/dependientesRoutes')(app, escogerBdd);
 
 
 const PORT = process.env.PORT || 5000;
