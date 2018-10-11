20938ffe286fb27a0d9a8f941859bc256014cf69
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.lan>
2018-10-11 01:38:30 -0500

    1

--- routes/dependientesRoutes.js
+++ routes/dependientesRoutes.js
@@ -30,8 +30,9 @@ module.exports = (app, escogerBdd) => {
 //=========================================================================
 
 	app.post('/api/dependientes/:equipo', escogerBdd, async (req, res) => {
-res.send(req.body._usuario);
-		// const { nombre_completo, edad, __usuario: _usuario, dependencia } = req.body;
+
+		const { nombre_completo, edad, _usuario, dependencia } = req.body;
+		console.log('_usuario: ', _usuario);
 
 		// if (!nombre_completo) res.send('Falta el nombre completo.');
 		// if (!dependencia) res.send('Falta dependencia.');
