064bebcb3ec2caaeba2167ac60cf58bd3184bcb3
Rodolfo Saldivar <rodolfo saldivar@TDG-MBL-CHA067.lan>
2018-10-10 23:51:09 -0500

    1

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const ponerCors = (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+		console.log('req.method: ', req.method);
 	if (req.method === 'OPTIONS') {
 		console.log('!OPTIONS');
 		var headers = {};
